0f48421f99dcc17bc4c2685d36745646dc7daa3f
Rich Siegel <rsiegel@ise.com>
2013-03-12 11:34:04 -0400

    properly set environment vars

--- make_pe/runtime/custom.ps1
+++ make_pe/runtime/custom.ps1
@@ -1,3 +1,3 @@
-puppet agent --pluginsync --no-daemonize --onetime --ignorecache --no-usecacheonfailure --show_diff --verbose --debug --environment production --tags winbuild
-Set $env:ise_mock_fqdn=pc-thin01.prod.ise.com
-X:\host-enforce.ps1 -b production -tags winbuild -disableeventlog true
+$env:ise_mock_fqdn="pc-thin01.prod.ise.com"
+$env:FACTER_env_windows_installdir="X:\puppet-2.7.x"
+X:\host-enforce.ps1 -b production -tags winbuild -disableeventlog true
