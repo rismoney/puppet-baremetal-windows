bbd4a6c9c17d4b8f5df2c6d7fcb243cb3494059f
Rich Siegel <rsiegel@ise.com>
2013-03-12 13:58:14 -0400

    add facter.d copy and pmaster add

--- make_pe/winbuild/build-wim.ps1
+++ make_pe/winbuild/build-wim.ps1
@@ -41,6 +41,10 @@ function build-wim {
   # patch puppet source to not do volume inspection for ntfs (X:\ is not a traditional volume) so we just say it is
   cmd /c "$mountfolder\patch\bin\patch.exe --force -d $mountfolder\puppet-2.7.x\lib\puppet\util\windows -p 0 < $patchfolder\security.rb.patch"
   
+  mkdir -p "$mountfolder\ProgramData\PuppetLabs\facter\facts.d"
+  echo d | xcopy /Y "$config\puppet_install.txt" "$mountfolder\ProgramData\PuppetLabs\facter\facts.d"
+  add-content "$mountfolder\ProgramData\PuppetLabs\facter\facts.d\puppet_install.txt" "`nfact_stomp_server=$puppetmaster"
+  
   #win pe startup scripts
   echo d |xcopy /Y "$runtimefolder\startnet.cmd" "$mountfolder\Windows\System32"
   echo d |xcopy /Y "$runtimefolder\custom.ps1" "$mountfolder"
