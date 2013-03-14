fe00ad32db01e31bece99e085c0c57b8f1464ce3
Rich Siegel <rsiegel@ise.com>
2013-03-14 17:13:07 -0400

    post puppet agent run placeholder files

--- make_pe/runtime/custom.ps1
+++ make_pe/runtime/custom.ps1
@@ -1,3 +1,4 @@
 $env:ise_mock_fqdn="pc-thin01.prod.ise.com"
 $env:FACTER_env_windows_installdir="X:\puppet-2.7.x"
 X:\host-enforce.ps1 -b production -tags winbuild -disableeventlog true
+X:\post-puppet.ps1
---
+++ make_pe/runtime/post-puppet.ps1
@@ -0,0 +1,1 @@
+# this is a placeholder file for puppet to inject work to do after the puppet run (ie setup.exe for build)
