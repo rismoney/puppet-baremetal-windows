6a19305a13264a63231cce656e28cf6aa37d0f16
Rich Siegel <rsiegel@ise.com>
2013-03-15 23:35:41 -0400

    use my branch and add facter var

--- make_pe/runtime/custom.ps1
+++ make_pe/runtime/custom.ps1
@@ -1,4 +1,5 @@
 $env:ise_mock_fqdn="pc-thin01.prod.ise.com"
 $env:FACTER_env_windows_installdir="X:\puppet-2.7.x"
-X:\host-enforce.ps1 -b production -tags winbuild -disableeventlog true
+$env:ise_kickstarting="yes"
+X:\host-enforce.ps1 -b rismoney_winbuild_baremetal -tags winbuild -disableeventlog true
 X:\post-puppet.ps1
