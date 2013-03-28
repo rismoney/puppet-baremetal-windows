855ff46c7d0d7b6e4f9bca6e29965379a459f7d5
Rich Siegel <rsiegel@ise.com>
2013-03-28 14:18:11 -0400

    added comma to tags for host-enforce

--- make_pe/runtime/custom.ps1
+++ make_pe/runtime/custom.ps1
@@ -1,5 +1,5 @@
 $env:ise_mock_fqdn="pc-thin01.prod.ise.com"
 $env:FACTER_env_windows_installdir="X:\puppet-2.7.x"
 $env:ise_kickstarting="yes"
-X:\host-enforce.ps1 -b rismoney_winbuild_baremetal -tags "winbuild choco ringo" -disableeventlog true
+X:\host-enforce.ps1 -b rismoney_winbuild_baremetal -tags "winbuild, choco, ringo" -disableeventlog true
 X:\post-puppet.ps1
