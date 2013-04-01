e663d297e803abe7f1e8eeef954b5bc539b234c3
Rich Siegel <rsiegel@ise.com>
2013-04-01 15:43:03 -0400

    leverage option 12 as a fqdn instead

--- make_pe/runtime/custom.ps1
+++ make_pe/runtime/custom.ps1
@@ -1,7 +1,8 @@
 Resolve-Path .\get-DHCPHostname.ps1 | % { . $_.ProviderPath }
 $hostname = (get-DHCPHostname)
-$dnsname = (gwmi win32_networkadapterconfiguration | where {$_.ipenabled -eq "true" -and $_.dhcpenabled -eq "true"}).dnsdomain
-$env:ise_mock_fqdn=$hostname + "." + $dnsname
+# $dnsname = (gwmi win32_networkadapterconfiguration | where {$_.ipenabled -eq "true" -and $_.dhcpenabled -eq "true"}).dnsdomain
+# $env:ise_mock_fqdn=$hostname + "." + $dnsname
+$env:ise_mock_fqdn = get-DHCPHostname
 $env:FACTER_env_windows_installdir="X:\puppet-2.7.x"
 $env:ise_kickstarting="yes"
 X:\host-enforce.ps1 -b rismoney_winbuild_baremetal -tags "winbuild, choco, ringo" -disableeventlog true
