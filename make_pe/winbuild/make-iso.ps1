2f9baa1d49b2bd624cfda88c6a519ff4e90016f5
Rich Siegel <rsiegel@ise.com>
2013-05-02 12:45:53 -0400

    remove boottime keyboard press

--- make_pe/winbuild/make-iso.ps1
+++ make_pe/winbuild/make-iso.ps1
@@ -3,9 +3,9 @@ function make-iso {
 # this needs cleanup
 #  lets prep the iso
   $isofolder=join-path $winpefolder "\ISO"
-  
+
   mkdir "$winpefolder\ISO"
-  
+
   xcopy /s "$adkfolder\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\Media" $isofolder
 
   # source bits into the iso
@@ -13,6 +13,9 @@ function make-iso {
   copy "$winpefolder\winpe.wim" "$isofolder\Sources\boot.wim"
   copy "$adkfolder\Assessment and Deployment Kit\Deployment Tools\x86\Oscdimg\oscdimg.exe" $winpefolder
   copy "$adkfolder\Assessment and Deployment Kit\Deployment Tools\x86\Oscdimg\etfsboot.com" $winpefolder
+
+  # we don't want a press any key to continue since we can't find ours :)
+  rm "$winpefolder\ISO\Boot\bootfix.bin"
 
   #lets make the iso (its about time)
 
