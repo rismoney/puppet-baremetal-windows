52303d64c71070ea0f31774017cfd4218c9643da
Rich Siegel <rsiegel@ise.com>
2013-05-02 23:01:19 -0400

    disable all adapters except for ethernet

--- make_pe/runtime/custom.ps1
+++ make_pe/runtime/custom.ps1
@@ -1,4 +1,6 @@
 . X:\get-DHCPHostname.ps1
+#disable all nics except "Ethernet"  windows 2012 is deterministic...
+gwmi -class win32_networkadapter |where-object {$_.netconnectionid -ne "Ethernet" -and $_.netenabled -eq $true} |foreach {$_.disable()}
 $hostname = (get-DHCPHostname)
 # $dnsname = (gwmi win32_networkadapterconfiguration | where {$_.ipenabled -eq "true" -and $_.dhcpenabled -eq "true"}).dnsdomain
 # $env:ise_mock_fqdn=$hostname + "." + $dnsname
