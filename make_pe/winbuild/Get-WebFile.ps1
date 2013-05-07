b0b4bc3682e7f40aaad3f238e9f846ed92ba8731
Rich Siegel <rsiegel@ise.com>
2013-05-07 11:36:03 -0400

    replace get-webfile with choco version

--- make_pe/winbuild/Get-WebFile.ps1
+++ make_pe/winbuild/Get-WebFile.ps1
@@ -1,19 +1,8 @@
-## Function Get-WebFile (aka wget for PowerShell)
-## Author: Joel Bennett and
-## Peter Kriegel http://www.admin-source.de
-## Original : http://poshcode.org/417
-##
+﻿# http://poshcode.org/417
+## Get-WebFile (aka wget for PowerShell)
 ##############################################################################################################
 ## Downloads a file or page from the web
 ## History:
-## v3.7 - Add Proxy support, check if a proxy is required and get credentials for it
-##        -ProxyAdress to Set the Adress of a Proxy Server
-##        -ProxyPort to Set the Port of a Proxy Server
-##        -ProxyUserName to Set the Username for the Proxy Server
-##        -ProxyUserPassword to Set the Password for the Proxy Server
-##        -ProxyUserDomain to Set the Domain which is used with the user for the Proxy Server
-##        -$userAgent cookie container to prevent the error with too many automatic redirections using a httpWebRequest in .NET
-##        Author: Peter Kriegel
 ## v3.6 - Add -Passthru switch to output TEXT files
 ## v3.5 - Add -Quiet switch to turn off the progress reports ...
 ## v3.4 - Add progress report for files which don't report size
@@ -25,143 +14,102 @@
 ## v2   - adds a ton of parsing to make the output pretty
 ##        added measuring the scripts involved in the command, (uses Tokenizer)
 ##############################################################################################################
-Function Get-WebFile {
-   [CmdletBinding(SupportsShouldProcess=$False)]
-   param(
-      [Parameter(Mandatory=$true)]
-      [String]$url,
-      [Parameter(Mandatory=$false)]
-      [String]$fileName,
-          [String]$ProxyAdress = $Null,
-          [Int]$ProxyPort = 0,
-          [String]$ProxyUserName = '',
-          [String]$ProxyUserPassword = '',
-          [String]$ProxyUserDomain = '',
-          [String]$userAgent = 'JoelBennett417Get-WebFile',
-      [switch]$Passthru,
-      [switch]$quiet
-   )
- 
-   $req = [System.Net.HttpWebRequest]::Create($url);
-       
-        $webclient = new-object System.Net.WebClient
-       
-        If($ProxyAdress.length -eq 0){ # no Proxy is given by the User
-                # check if a proxy is required
-                If (!$webclient.Proxy.IsBypassed($url)) {
-                        $creds = [net.CredentialCache]::DefaultCredentials
-                    if ($creds -eq $null) {
-                      Write-Debug "Default credentials were null. Attempting backup method"
-                      $cred = get-credential
-                      $creds = $cred.GetNetworkCredential()
-                    }
-                    $proxyaddress = $webclient.Proxy.GetProxy($url).Authority
-                    Write-Verbose "Using this proxyserver: $proxyaddress"
-                    $proxy = New-Object System.Net.WebProxy($proxyaddress)
-                    $proxy.credentials = $creds
-                    $req.proxy = $proxy
-                }
-        }
-        Else { # Proxy was given by the User
-                 If($ProxyPort -gt 0){
-                                $proxy = New-Object System.Net.WebProxy($ProxyAdress,$ProxyPort)
-                        }
-                        Else {
-                                $proxy = New-Object System.Net.WebProxy($ProxyAdress)
-                        }
-                       
-                        If($ProxyUserName.length -gt 0){
-                                # if username is given we create the credentials
-                                $creds = New-Object System.Net.NetworkCredential($ProxyUserName,$ProxyUserPassword,$ProxyUserDomain)   
-                        }
-                        Else {
-                                # if no username is given try to get the default credentials
-                                $creds = [net.CredentialCache]::DefaultCredentials
-                        }
-                       
-                    if ($creds -eq $null) {
-                          # no credentials given or found so we as for it
-                      Write-Debug "Default credentials were null. Attempting backup method"
-                      $cred = get-credential
-                      $creds = $cred.GetNetworkCredential()
-                    }
-                       
-                        $proxy.credentials = $creds
-                    $req.proxy = $proxy
-        }
-       
-        #http://stackoverflow.com/questions/518181/too-many-automatic-redirections-were-attempted-error-message-when-using-a-httpw
-        $req.CookieContainer = New-Object System.Net.CookieContainer
-    if ($userAgent -ne $null) {
-         Write-Debug "Setting the UserAgent to `'$userAgent`'"
-         $req.UserAgent = $userAgent
+function Get-WebFile {
+param(
+  $url = '', #(Read-Host "The URL to download"),
+  $fileName = $null,
+  $userAgent = 'chocolatey command line',
+  [switch]$Passthru,
+  [switch]$quiet
+)
+  Write-Debug "Running 'Get-WebFile' for $fileName with url:`'$url`', userAgent: `'$userAgent`' ";
+  #if ($url -eq '' return)
+  $req = [System.Net.HttpWebRequest]::Create($url);
+  #to check if a proxy is required
+  $webclient = new-object System.Net.WebClient
+  if (!$webclient.Proxy.IsBypassed($url))
+  {
+    $creds = [net.CredentialCache]::DefaultCredentials
+    if ($creds -eq $null) {
+      Write-Debug "Default credentials were null. Attempting backup method"
+      $cred = get-credential
+      $creds = $cred.GetNetworkCredential();
     }
+    $proxyaddress = $webclient.Proxy.GetProxy($url).Authority
+    Write-host "Using this proxyserver: $proxyaddress"
+    $proxy = New-Object System.Net.WebProxy($proxyaddress)
+    $proxy.credentials = $creds
+    $req.proxy = $proxy
+  }
  
-    $res = $req.GetResponse();
- 
- 
-   if($fileName -and !(Split-Path $fileName)) {
-      $fileName = Join-Path (Get-Location -PSProvider "FileSystem") $fileName
-   }
-   elseif((!$Passthru -and ($fileName -eq $null)) -or (($fileName -ne $null) -and (Test-Path -PathType "Container" $fileName)))
-   {
-      [string]$fileName = ([regex]'(?i)filename=(.*)$').Match( $res.Headers["Content-Disposition"] ).Groups[1].Value
-      $fileName = $fileName.trim("\/""'")
-      if(!$fileName) {
-         $fileName = $res.ResponseUri.Segments[-1]
-         $fileName = $fileName.trim("\/")
-         if(!$fileName) {
-            $fileName = Read-Host "Please provide a file name"
-         }
-         $fileName = $fileName.trim("\/")
-         if(!([IO.FileInfo]$fileName).Extension) {
-            $fileName = $fileName + "." + $res.ContentType.Split(";")[0].Split("/")[1]
-         }
-      }
-      $fileName = Join-Path $env:TEMP $fileName
-   }
-   if($Passthru) {
-      $encoding = [System.Text.Encoding]::GetEncoding($res.CharacterSet ) # 'utf-8'
-      [string]$output = ""
-   }
- 
-   if($res.StatusCode -eq 200) {
-      [int]$goal = $res.ContentLength
-      $reader = $res.GetResponseStream()
-      if($fileName) {
-         $writer = new-object System.IO.FileStream $fileName, "Create"
-      }
-      [byte[]]$buffer = new-object byte[] 1048576
-      [int]$total = [int]$count = 0
-      do
-      {
-         $count = $reader.Read($buffer, 0, $buffer.Length);
-         if($fileName) {
-            $writer.Write($buffer, 0, $count);
-         }
-         if($Passthru){
-            $output += $encoding.GetString($buffer,0,$count)
-         } elseif(!$quiet) {
-            $total += $count
-            if($goal -gt 0) {
-               Write-Progress "Downloading $url" "Saving $total of $goal" -id 0 -percentComplete (($total/$goal)*100)
-            } else {
-               Write-Progress "Downloading $url" "Saving $total bytes..." -id 0
-            }
-         }
-      } while ($count -gt 0)
- 
-      $reader.Close()
-      if($fileName) {
-         $writer.Flush()
-         $writer.Close()
-      }
-      if($Passthru){
-         $output
-      }
-   }
-   $res.Close();
-   if($fileName) {
-      ls $fileName
-   }
+  #http://stackoverflow.com/questions/518181/too-many-automatic-redirections-were-attempted-error-message-when-using-a-httpw
+  $req.CookieContainer = New-Object System.Net.CookieContainer
+  if ($userAgent -ne $null) {
+    Write-Debug "Setting the UserAgent to `'$userAgent`'"
+    $req.UserAgent = $userAgent
+  }
+  $res = $req.GetResponse();
+
+  if($fileName -and !(Split-Path $fileName)) {
+    $fileName = Join-Path (Get-Location -PSProvider "FileSystem") $fileName
+  }
+  elseif((!$Passthru -and ($fileName -eq $null)) -or (($fileName -ne $null) -and (Test-Path -PathType "Container" $fileName)))
+  {
+    [string]$fileName = ([regex]'(?i)filename=(.*)$').Match( $res.Headers["Content-Disposition"] ).Groups[1].Value
+    $fileName = $fileName.trim("\/""'")
+    if(!$fileName) {
+       $fileName = $res.ResponseUri.Segments[-1]
+       $fileName = $fileName.trim("\/")
+       if(!$fileName) {
+          $fileName = Read-Host "Please provide a file name"
+       }
+       $fileName = $fileName.trim("\/")
+       if(!([IO.FileInfo]$fileName).Extension) {
+          $fileName = $fileName + "." + $res.ContentType.Split(";")[0].Split("/")[1]
+       }
+    }
+    $fileName = Join-Path (Get-Location -PSProvider "FileSystem") $fileName
+  }
+  if($Passthru) {
+    $encoding = [System.Text.Encoding]::GetEncoding( $res.CharacterSet )
+    [string]$output = ""
+  }
+
+  if($res.StatusCode -eq 200) {
+    [long]$goal = $res.ContentLength
+    $reader = $res.GetResponseStream()
+    if($fileName) {
+       $writer = new-object System.IO.FileStream $fileName, "Create"
+    }
+    [byte[]]$buffer = new-object byte[] 1048576
+    [long]$total = [long]$count = [long]$iterLoop =0
+    do
+    {
+       $count = $reader.Read($buffer, 0, $buffer.Length);
+       if($fileName) {
+          $writer.Write($buffer, 0, $count);
+       }
+       if($Passthru){
+          $output += $encoding.GetString($buffer,0,$count)
+       } elseif(!$quiet) {
+          $total += $count
+          if($goal -gt 0 -and ++$iterLoop%10 -eq 0) {
+             Write-Progress "Downloading $url to $fileName" "Saving $total of $goal" -id 0 -percentComplete (($total/$goal)*100) 
+          }
+          if ($total -eq $goal) {
+            Write-Progress "Completed download of $url." "Completed a total of $total bytes of $fileName" -id 0 -Completed 
+          }
+       }
+    } while ($count -gt 0)
+   
+    $reader.Close()
+    if($fileName) {
+       $writer.Flush()
+       $writer.Close()
+    }
+    if($Passthru){
+       $output
+    }
+  }
+  $res.Close();
 }
