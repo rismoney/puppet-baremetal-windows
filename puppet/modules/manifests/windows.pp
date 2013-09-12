780eb15474a80f069ab5d68ee9c7f7cef935dc98
Rich Siegel <rsiegel@ise.com>
2013-09-12 14:15:52 -0400

    add more complex logic to winbuild manifest
    specifically:
    * support windows 2008 and windows 2012
    * provide example class handling for specific h/w virtualization
    scenarios
    * leverage hiera based values
    
    note- this expects facter facts for productname and ise_kickstarting

--- puppet/modules/manifests/windows.pp
+++ puppet/modules/manifests/windows.pp
@@ -1,5 +1,28 @@
 class winbuild::windows {
-  if $::ise_mock_function == 'thin' {
-    include winbuild::baremetal
+
+
+  # if not in hiera fallback to Windows 2008
+  $targetos = hiera('targetos','win2008')
+
+  $imageno = $targetos ? {
+    'win2008' => '1',# standard edition image on 2008
+    'win2012' => '2',# standard edition image on 2012
+    default     => '1',
+  }
+
+  if $::ise_kickstarting == 'yes' {
+    class {'winbuild::baremetal':}
+    -> class {"winbuild::${targetos}":}
+  }
+
+  case $::productname {
+    'VMware Virtual Platform': {
+      include winbuild::vmware
+    }
+    'ProLiant BL460c G6',
+    'ProLiant BL460c Gen8': {
+      include winbuild::hpserver
+    }
+    default: { include winbuild::generic } # apply the generic class
   }
 }
