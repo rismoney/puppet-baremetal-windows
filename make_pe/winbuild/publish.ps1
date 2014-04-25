4a7468fd50347808bc6bbd0c921dfcf69f5e5390
Rich Siegel <rismoney@gmail.com>
2014-04-25 18:06:25 +0000

    publish to web site and add md5sum

---
+++ make_pe/winbuild/publish.ps1
@@ -0,0 +1,3 @@
+(Get-FileHash C:\WinPE_x86\WinPE_x86.iso).Hash | out-file C:\WinPE_x86\WinPE_x86.iso.md5
+C:\WinPE_x86\WinPE_x86.iso.md5 C:\inetpub\wwwroot
+C:\WinPE_x86\WinPE_x86.iso C:\inetpub\wwwroot
