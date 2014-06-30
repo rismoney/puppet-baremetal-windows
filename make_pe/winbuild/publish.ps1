c2c7baeb13abd2400565b5c93835336560a94df7
Rich Siegel <rsiegel@ise.com>
2014-06-30 15:37:40 -0400

    constrain publish to PS v4 or greater and add missing copy cmds

--- make_pe/winbuild/publish.ps1
+++ make_pe/winbuild/publish.ps1
@@ -1,3 +1,5 @@
-(Get-FileHash C:\WinPE_x86\WinPE_x86.iso).Hash | out-file C:\WinPE_x86\WinPE_x86.iso.md5
-C:\WinPE_x86\WinPE_x86.iso.md5 C:\inetpub\wwwroot
-C:\WinPE_x86\WinPE_x86.iso C:\inetpub\wwwroot
+if ($PSVersionTable.PSVersion.Major -ge 4) {
+  (Get-FileHash C:\WinPE_x86\WinPE_x86.iso).Hash | out-file C:\WinPE_x86\WinPE_x86.iso.md5
+  copy C:\WinPE_x86\WinPE_x86.iso.md5 C:\inetpub\wwwroot
+  copy C:\WinPE_x86\WinPE_x86.iso C:\inetpub\wwwroot
+}
