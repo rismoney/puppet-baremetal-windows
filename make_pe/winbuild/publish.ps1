37eb741f802ba5c198177051e734170573c1aba5
Rich Siegel <rsiegel@ise.com>
2015-10-01 11:31:37 -0400

    remove presumptive folder copying

--- make_pe/winbuild/publish.ps1
+++ make_pe/winbuild/publish.ps1
@@ -1,5 +1,5 @@
 if ($PSVersionTable.PSVersion.Major -ge 4) {
-  (Get-FileHash C:\WinPE_x86\WinPE_x86.iso).Hash | out-file C:\WinPE_x86\WinPE_x86.iso.md5
-  copy C:\WinPE_x86\WinPE_x86.iso.md5 C:\inetpub\wwwroot
-  copy C:\WinPE_x86\WinPE_x86.iso C:\inetpub\wwwroot
+  if (test-path C:\WinPE_x86\WinPE_x86.iso) {
+    (Get-FileHash C:\WinPE_x86\WinPE_x86.iso).Hash | out-file C:\WinPE_x86\WinPE_x86.iso.md5
+  }
 }
